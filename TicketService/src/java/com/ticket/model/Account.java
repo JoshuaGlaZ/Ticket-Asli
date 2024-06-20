cad8515baa63ef9cc8d9a33da6b6b71450b8aec3
JoshuaGlaZ <joshuaminex02@gmail.com>
2024-06-20 14:39:18 +0700

    Fix model -> server

---
+++ TicketService/src/java/com/ticket/model/Account.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ticket.model;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Account extends MyModel {
+
+    private int id;
+    private String username;
+    private String password;
+    private String email; 
+    private Timestamp created_at;
+    
+    public Account() {
+        this.id = 0;
+        this.username = null;
+        this.password = null;
+        this.email = null;
+        this.created_at = null;
+    }
+
+    public Account(int id, String username, String password, String email, Timestamp created_at) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.created_at = created_at;
+    }    
+
+    public Account(String username, String password, String email) {
+        this.id = 0;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.created_at = null;
+    }    
+            
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+   
+    public boolean checkEmail() {
+        for (Object object : viewListData()) {
+            Account a = (Account)object;
+            if (a.getUsername().equals(this.getUsername())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void insertData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = (PreparedStatement) MyModel.conn.prepareStatement(
+                        "INSERT INTO account(username, password, email) "
+                                + "VALUES (?, ?, ?)");
+                sql.setString(1, this.username);
+                sql.setString(2, this.password);
+                sql.setString(3, this.email);
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error di insertData account: " + e);
+        }
+    }
+
+    @Override
+    public void updateData() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deleteData() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Object> viewListData() {
+        ArrayList<Object> collections = new ArrayList<>();
+        try {
+            this.statement = (Statement) MyModel.conn.createStatement();
+            this.result = this.statement.executeQuery("SELECT * FROM account;");
+            while (this.result.next()) {
+                Account a = new Account(
+                        this.result.getInt("id"),
+                        this.result.getString("username"),
+                        this.result.getString("password"),
+                        this.result.getString("email"),
+                        this.result.getTimestamp("created_at")
+                );
+                collections.add(a);
+            }
+        } catch (Exception e) {
+            System.out.println("Error di viewListData: " + e);
+        }
+            
+        return collections;
+    }
+    
+    
+    
+}
--- TicketService/src/java/com/ticket/model/MyModel.java
+++ TicketService/src/java/com/ticket/model/MyModel.java
@@ -2,17 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package parking.model;
+package com.ticket.model;
 
-import java.beans.Statement;
-import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
  *
- * @author chris
+ * @author LENOVO
  */
 public abstract class MyModel {
 
@@ -21,26 +21,30 @@ public abstract class MyModel {
     protected ResultSet result;
 
     public MyModel() {
-        this.conn = this._getconnection();
+        this.conn = this._getConnection();
         this.statement = null;
         this.result = null;
     }
 
-    private Connection _getconnection() {
+    private Connection _getConnection() {
         if (MyModel.conn == null) {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-                return DriverManager.getConnection("jdbc:mysql://kresnayangasli.my.id/tiket_asli", "dp_uas", "dp*1234567890"); //belum ada databasenya
+                return DriverManager.getConnection("jdbc:mysql://kresnayangasli.my.id:47780/tiket_asli", 
+                        "dp_uas", "dp*1234567890");
             } catch (Exception e) {
-                System.out.println("Error di getConnection : " + e);
+                System.out.println("Error di getConnection: " + e);
             }
         }
         return MyModel.conn;
     }
 
     public abstract void insertData();
+
     public abstract void updateData();
+
     public abstract void deleteData();
+
     public abstract ArrayList<Object> viewListData();
 
 }
